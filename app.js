4160afb2e795f0ae8de18be02fa2d4b7610383f6
Pilner <raileyvictuelles@gmail.com>
2023-05-12 22:45:37 +0800

    ADDED Initial Version +Started on Home, Add pages +Added POST request for inputting data from a form +Implemented the Insertion of data if it is completely unique +Others

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // SETUP
 import * as dotenv from "dotenv";
 import express from "express";
+import {createTable, addData} from "./sqliteFunctions.mjs";
 
 dotenv.config();
 
-const	app 	= express(),
-		port	= 3000 || process.env.PORT;
+const app = express(),
+  port = 3000 || process.env.PORT;
 
 
 app.use(express.urlencoded({extended: true}));
@@ -14,20 +15,53 @@ app.use(express.static("public"));
 
 // ROUTES
 app.get("/", (req, res) => {
-	try {
-		res.render("home");
-	} catch(err) {
-		res.render("errorpage");
-		console.log(err);
-	}
-})
+  try {
+    res.render("home");
+  } catch (err) {
+    res.render("errorpage");
+    console.error(err);
+  }
+});
+
+app.get("/add", (req, res) => {
+  try {
+    res.render("addpage")
+  } catch (err) {
+    res.render("errorpage");
+    console.error(err);
+  }
+});
+
+app.get("/view", (req, res) => {
+  try {
+    res.render("viewpage")
+  } catch (err) {
+    res.render("errorpage");
+    console.error(err);
+  }
+});
+
+app.get("/about", (req, res) => {
+  try {
+    res.render("aboutpage")
+  } catch (err) {
+    res.render("errorpage");
+    console.error(err);
+  }
+});
 
 app.get("*", (req, res) => {
-	res.render("errorpage");
-})
+  res.render("errorpage");
+});
+
+
+app.post("/post-test", (req, res) => {
+  console.log('Got body: ', req.body);
+  addData(req.body.name, req.body.album, req.body.artist);
+});
 
 
 // LISTEN
 app.listen(port, () => {
-	console.log(`Server starting on port ${port}`);
+  console.log(`Server starting on port ${port}`);
 })
---
+++ sqliteFunctions.mjs
@@ -0,0 +1,167 @@
+import sqlite3 from "sqlite3";
+
+function insertAlbum(album, artist) {
+  return new Promise((resolve, reject) => {
+
+    let db = new sqlite3.Database("./db/1chinook.db", sqlite3.OPEN_READWRITE, (err) => {
+      if (err) {
+        console.error(err.message);
+        reject(err);
+      }
+      let artistId;
+      db.get(`select artistId from artists where artists.Name = (?)`, [artist], (err, row) => {
+        if (err) {
+          console.err(err.message);
+        } else {
+          if (row) {
+            artistId = row.ArtistId;
+          }
+          db.run(`
+    insert into albums(Title, artistId) values (?, ?)
+  `, [album, artistId], (err) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log(`Inserted into albums`);
+            }
+          })
+          db.close(err => {
+            if (err) {
+              console.error(err.message);
+              reject(err);
+            } else {
+              console.log("(2) Albums Done!")
+              resolve();
+            };
+          })
+        }
+      })
+    });
+  })
+}
+
+function insertSong(song, name) {
+  return new Promise((resolve, reject) => {
+    let db = new sqlite3.Database("./db/1chinook.db", sqlite3.OPEN_READWRITE, (err) => {
+      if (err) {
+        console.error(err.message);
+        reject(err);
+      }
+      let albumId;
+      db.get(`select * from albums where Title = (?)`, [name], (err, row) => {
+        if (err) {
+          console.error(err);
+        } else {
+          if (row) {
+            albumId = row.AlbumId;
+          }
+
+          let randMTI = Math.floor(Math.random() * 5) + 1;
+          let randMS = Math.floor(Math.random() * (500000 - 100000 + 1)) + 100000;
+          let randUP = Math.floor(Math.random() * (5.99 - 0.99 + 1)) + 0.99;
+          db.run(`
+    insert into tracks(Name, AlbumId, MediaTypeId, Milliseconds, UnitPrice) values (?, ?, ?, ?, ?)
+  `, [song, albumId, randMTI, randMS, randUP], (err) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log(`Inserted into tracks`);
+            }
+          })
+          db.close(err => {
+            if (err) {
+              console.error(err.message);
+              reject(err);
+            } else {
+              console.log("(3) Tracks Done!")
+              resolve();
+            };
+          })
+        }
+      })
+
+
+
+    });
+  })
+}
+
+async function insertArtist(artist) {
+  return new Promise((resolve, reject) => {
+    let db = new sqlite3.Database("./db/1chinook.db", sqlite3.OPEN_READWRITE, (err) => {
+      if (err) {
+        console.error(err.message);
+      }
+      db.run(`
+    insert into artists(Name) values (?)
+  `, [artist], (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`Inserted into artists`);
+        }
+      })
+      db.close(err => {
+        if (err) {
+          console.error(err.message);
+        } else {
+          console.log("(1) Artists Done!")
+          resolve();
+        };
+      })
+    });
+  })
+}
+
+export async function addData(name, album, artist) {
+  try {
+    let db = new sqlite3.Database("./db/1chinook.db", sqlite3.OPEN_READWRITE);
+
+    let row = await new Promise((resolve, reject) => {
+      db.get(`
+        SELECT *
+        FROM albums
+        JOIN artists USING (ArtistId)
+        JOIN tracks USING (AlbumId)
+        WHERE tracks.Name = ?
+      `, [name], (err, row) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        }
+        resolve(row);
+      });
+    });
+
+    if (row) {
+      await new Promise((resolve, reject) => {
+        db.run(`
+          INSERT INTO tracks (Name) VALUES (?)
+        `, [name], (err) => {
+          if (err) {
+            console.error(err);
+            reject(err);
+          }
+          resolve();
+        });
+      });
+
+    } else {
+      await insertArtist(artist);
+      await insertAlbum(album, artist);
+      await insertSong(name, album);
+      console.log("Insertion Complete!");
+    }
+
+    db.close((err) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log("Add Data Done!");
+      }
+    });
+
+  } catch (err) {
+    console.error(err.message);
+  }
+}
